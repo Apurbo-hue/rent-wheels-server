d708d77aee3f3b9cd6f8ccde2ef150b908d2ee66
Apurbo-hue <labunna123@gmail.com>
2026-06-07 23:10:29 +0600

    MongoDB connection setup with environment variables

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jhmuzak.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("MongoDB connected");
+        const db = await client.db("rent_wheels_db");
+        const usersCollection = await db.collection("users");
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged successfully");
+    }
+    finally {
+        
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
