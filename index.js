d711134f4f2dfbf73466e85ed0405f5f4025b9c6
Apurbo-hue <labunna123@gmail.com>
2026-06-11 07:59:58 +0600

    MongoDB get post tested

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jhmuzak.mongodb.net/?appName=Cluster0`;
 // Create a MongoClient 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
@@ -24,11 +24,33 @@ async function run() {
         console.log("MongoDB connected");
         const db = await client.db("rent_wheels_db");
         const usersCollection = await db.collection("users");
+
+        //get the users
+        app.get("/users", async (req, res) => {
+            const cursor = usersCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        //post a new user
+        app.post("/users", async (req, res) => {
+            const newUser = req.body;
+            const email = req.body.email;
+            const query = { email:email};
+            const checkUser = await usersCollection.findOne(query)
+            if (checkUser) {
+                res.send({ message: "User already exists" })
+            }
+            else {
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result);
+            }
+        })
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged successfully");
     }
     finally {
-        
+
     }
 }
 
